327cdbb545147368a808d856fca0e33cd10137fe
Abhi86shek <sonkar.abhishek86@gmail.com>
2021-02-20 21:54:27 +0530

    Added some utility actions

---
+++ Utilities/Actions/DeactivateObjectOnStateExit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SAS.StateMachineGraph.Utilities
+{
+    public sealed class ActivateObjectOnStateExit : IStateInitialize, IStateExit
+    {
+        private Transform _transform;
+        void IStateInitialize.OnInitialize(Actor actor, string tag)
+        {
+            if (!actor.TryGet(tag, out _transform, true))
+                Debug.LogError($"No GameObject with tag {tag} is found usder {actor}. Try assigning the Tag");
+        }
+
+       void IStateExit.OnStateExit(Actor actor)
+        {
+            _transform?.gameObject.SetActive(true);
+        }
+    }
+}
