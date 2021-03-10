14c314255542c81e5b9265febbcd541265e3f376
Abhi86shek <sonkar.abhishek86@gmail.com>
2021-03-10 17:43:24 +0530

    Adding scrptable Type support

--- Runtime/Utilities/Actions/ApplyActorParameter.cs
+++ Runtime/Utilities/Actions/ApplyActorParameter.cs
@@ -2,22 +2,21 @@
 
 namespace SAS.StateMachineGraph.Utilities
 {
-    public class ApplyActorParameter : IStateInitialize, IStateEnter
+    public struct ApplyActorParameter : IStateInitialize, IStateEnter
     {
         private Actor[] _actors;
-        private Parameter _parameter;
+        private string _parameterKey;
         public void OnInitialize(Actor actor, string tag, string key)
         {
-            actor.Get<ParameterConfig>(tag).TryGet(key, out _parameter);
-            
+            _parameterKey = key;
             if (!actor.TryGetAll(tag, out _actors, true))
                 Debug.LogError($"No Actor with tag {tag} is found usder {actor}. Try assigning the Tag");
         }
 
         public void OnStateEnter(Actor actor)
         {
-            foreach (var childActor in _actors)
-                childActor.Apply(in _parameter);
+            foreach (var cActor in _actors)
+                cActor.Apply(cActor.Get<ScriptableStateMachineParameter>(_parameterKey).runtimeValue);
         }
     }
 }
---
+++ Runtime/Utilities/ScriptableStateMachineParameter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SAS.StateMachineGraph.Utilities
+{
+    [CreateAssetMenu(menuName = "SAS/State Machine Controller/ScriptableParameter")]
+    public class ScriptableStateMachineParameter : ScriptableObject, ISerializationCallbackReceiver
+    {
+        [SerializeField] private Parameter m_InitialValue = default;
+        [NonSerialized] public Parameter runtimeValue;
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+           // throw new NotImplementedException();
+        }
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            runtimeValue = m_InitialValue;
+        }
+    }
+}
