2baff5a9d8e8efd75d2c1c7b3f33f5c18e853cfd
Abhi86shek <sonkar.abhishek86@gmail.com>
2021-03-13 22:18:51 +0530

    UnRegister the stateEnter/Exit event once executed

--- Runtime/Utilities/Actions/ObserveChildActorStateEnter.cs
+++ Runtime/Utilities/Actions/ObserveChildActorStateEnter.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace SAS.StateMachineGraph.Utilities
 {
@@ -7,21 +8,25 @@ namespace SAS.StateMachineGraph.Utilities
         private Actor _childActor;
         public bool IsCompleted { get; set; }
         private string _stateName;
+        private Action<string> _onStateEnter;
 
         public void OnInitialize(Actor actor, string tag, string key)
         {
             _stateName = key;
             if (!actor.TryGet(tag, out _childActor, true))
                 Debug.LogError($"No Actor with tag {tag} is found under {actor.name} attached on the object {actor.gameObject.name}. Try assigning the Tag");
-
-            _childActor.OnStateEnter += (state) =>
-            {
-                IsCompleted = state.Equals(_stateName);
-            };
         }
 
         public void Execute(Actor actor)
         {
+            IsCompleted = false;
+            _onStateEnter = (state) =>
+            {
+                IsCompleted = state.Equals(_stateName);
+                _childActor.OnStateEnter -= _onStateEnter;
+            };
+
+            _childActor.OnStateEnter += _onStateEnter;
         }
     }
 }
--- Runtime/Utilities/Actions/ObserveChildActorStateExit.cs
+++ Runtime/Utilities/Actions/ObserveChildActorStateExit.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace SAS.StateMachineGraph.Utilities
 {
@@ -7,21 +8,25 @@ namespace SAS.StateMachineGraph.Utilities
         private Actor _childActor;
         public bool IsCompleted { get; set; }
         private string _stateName;
+        private Action<string> _onStateExit;
 
         public void OnInitialize(Actor actor, string tag, string key)
         {
             _stateName = key;
             if (!actor.TryGet(tag, out _childActor, true))
                 Debug.LogError($"No Actor with tag {tag} is found under {actor.name} attached on the object {actor.gameObject.name}. Try assigning the Tag");
-
-            _childActor.OnStateExit += (state) =>
-            {
-                IsCompleted = state.Equals(_stateName);
-            };
         }
 
         public void Execute(Actor actor)
         {
+            IsCompleted = false;
+            _onStateExit = (state) =>
+            {
+                IsCompleted = state.Equals(_stateName);
+                _childActor.OnStateEnter -= _onStateExit;
+            };
+
+            _childActor.OnStateEnter += _onStateExit;
         }
     }
 }
