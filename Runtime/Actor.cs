cf268f7ef296465d72dd3d3211cc1a217afc6037
Abhi86shek <sonkar.abhishek86@gmail.com>
2021-10-22 01:24:14 +0530

    Clean-up and refactoring

--- Runtime/Actor.cs
+++ Runtime/Actor.cs
@@ -25,7 +25,6 @@ namespace SAS.StateMachineGraph
         [SerializeField] private Config[] m_Configs = default;
 
         internal StateMachine StateMachineController { get; private set; }
-        private readonly ServiceLocator _serviceLocator = new ServiceLocator();
         public string CurrentStateName => StateMachineController?.CurrentState?.Name;
         public State CurrentState => StateMachineController?.CurrentState;
         private bool _isConfigsCached = false;
@@ -41,9 +40,15 @@ namespace SAS.StateMachineGraph
             if (!_isConfigsCached)
             {
                 foreach (var config in m_Configs)
-                    _serviceLocator.Add(config.data.GetType(), config.data, config.name);
+                    ComponentExtensions.serviceLocator.Add(config.data.GetType(), config.data, config.name);
                 _isConfigsCached = true;
             }
+        }
+
+        public bool TryGet<T>(out T service, string tag = "")
+        {
+            CacheConfig();
+            return ComponentExtensions.serviceLocator.TryGet<T>(out service, tag);
         }
 
         private void Initialize()
@@ -70,124 +75,6 @@ namespace SAS.StateMachineGraph
         {
             StateMachineController?.OnLateUpdate();
             StateMachineController?.TryTransition(OnStateChanged);
-        }
-
-        public T Get<T>(string tag = "")
-        {
-            if (TryGet<T>(out var service, tag))
-                return service;
-
-            return default(T);
-        }
-
-        public void Add<T>(object service, string tag = "")
-        {
-            _serviceLocator.Add<T>(service, tag);
-        }
-
-        public bool TryGet<T>(out T service, string tag = "") 
-        {
-            CacheConfig();
-            return _serviceLocator.TryGet<T>(out service, tag);
-        }
-
-        public T GetOrCreate<T>(string tag = "")
-        {
-            return _serviceLocator.GetOrCreate<T>(tag);
-        }
-
-        public bool Remove<T>(string tag = "")
-        {
-            return _serviceLocator.Remove<T>(tag);
-        }
-
-        public bool TryGetComponent<T>(out T component, string tag = "")
-        {
-            component = (T)(object)GetComponent(typeof(T), tag);
-            return component != null;
-        }
-
-        public bool TryGetComponentInChildren<T>(out T component, string tag = "", bool includeInactive = false)
-        {
-            component = (T)(object)GetComponentInChildren(typeof(T), tag, includeInactive);
-            return component != null;
-        }
-
-        public Component GetComponent(Type type, string tag = "")
-        {
-            var obj = TaggerExtensions.GetComponent(this, type, tag);
-            if (obj == null)
-                Debug.LogError($"No component of type {type.Name} with tag {tag} is found under actor {this}, attached on the game object {gameObject.name}. Try assigning the component with the right Tag");
-
-            return obj;
-        }
-
-        public Component GetComponentInChildren(Type type, string tag = "", bool includeInactive = false)
-        {
-            var obj = TaggerExtensions.GetComponentInChildren(this, type, tag, includeInactive);
-            if (obj == null)
-                Debug.LogError($"No component of type {type.Name} with tag {tag} is found under actor {this}, attached on the game object {gameObject.name}. Try assigning the component with the right Tag");
-
-            return obj;
-        }
-
-        public bool TryGetComponentsInChildren<T>(out T[] components, string tag, bool includeInactive = false)
-        {
-            var results = this.GetComponentsInChildren(typeof(T), tag, includeInactive);
-            try
-            {
-                if (results.Length == 0)
-                    results = null;
-
-                components = new T[results.Length];
-                for (int i = 0; i < results.Length; ++i)
-                    components[i] = (T)(object)results[i];
-            }
-            catch (Exception)
-            {
-                components = null;
-                Debug.LogError($"No component of type {components.GetType()} with tag {tag} is found under actor {this}, attached on the game object {gameObject.name}. Try assigning the component with the right Tag");
-                return false;
-            }
-
-            return true;
-        }
-
-        public bool TryGetComponentsInParent<T>(out T[] components, string tag = "", bool includeInactive = false)
-        {
-            var results = this.GetComponentsInParent(typeof(T), tag, includeInactive);
-            try
-            {
-                if (results.Length == 0)
-                    results = null;
-
-                components = new T[results.Length];
-                for (int i = 0; i < results.Length; ++i)
-                    components[i] = (T)(object)results[i];
-            }
-            catch (Exception)
-            {
-                components = null;
-                Debug.LogError($"No component of type {components.GetType()} with tag {tag} is found in parent of actor {this}, attached on the game object {gameObject.name}. Try assigning the component with the right Tag");
-                return false;
-            }
-
-            return true;
-        }
-
-        public bool TryGetComponentInParent<T>(out T component, string tag = "", bool includeInactive = false)
-        {
-            component = (T)(object)GetComponentInParent(typeof(T), tag, includeInactive);
-            return component != null;
-        }
-
-        public Component GetComponentInParent(Type type, string tag = "", bool includeInactive = false)
-        {
-            var obj = TaggerExtensions.GetComponentInParent(this, type, tag, includeInactive);
-            if (obj == null)
-                Debug.LogError($"No component of type {type.GetType()} with tag {tag} is found under actor {this}, attached on the game object {gameObject.name}. Try assigning the component with the right Tag");
-
-            return obj;
         }
 
         public void SetFloat(string name, float value)
---
+++ Runtime/ActorExtensions.cs
@@ -0,0 +1,121 @@
+using SAS.TagSystem;
+using System;
+using UnityEngine;
+
+namespace SAS.StateMachineGraph
+{
+    public static class ActorExtensions
+    {
+        public static bool TryGetComponent<T>(this Actor actor, out T component, string tag = "")
+        {
+            component = (T)(object)actor.GetComponent(typeof(T), tag);
+            return component != null;
+        }
+
+        public static bool TryGetComponentInChildren<T>(this Actor actor, out T component, string tag = "", bool includeInactive = false)
+        {
+            component = (T)(object)actor.GetComponentInChildren(typeof(T), tag, includeInactive);
+            return component != null;
+        }
+
+        public static Component GetComponent(this Actor actor, Type type, string tag = "")
+        {
+            var obj = TaggerExtensions.GetComponent(actor, type, tag);
+            if (obj == null)
+                Debug.LogError($"No component of type {type.Name} with tag {tag} is found under actor {actor.name}, attached on the game object {actor.gameObject.name}. Try assigning the component with the right Tag");
+
+            return obj;
+        }
+
+        public static Component GetComponentInChildren(this Actor actor, Type type, string tag = "", bool includeInactive = false)
+        {
+            var obj = TaggerExtensions.GetComponentInChildren(actor, type, tag, includeInactive);
+            if (obj == null)
+                Debug.LogError($"No component of type {type.Name} with tag {tag} is found under actor {actor.name}, attached on the game object {actor.gameObject.name}. Try assigning the component with the right Tag");
+
+            return obj;
+        }
+
+        public static bool TryGetComponentsInChildren<T>(this Actor actor, out T[] components, string tag, bool includeInactive = false)
+        {
+            var results = actor.GetComponentsInChildren(typeof(T), tag, includeInactive);
+            try
+            {
+                if (results.Length == 0)
+                    results = null;
+
+                components = new T[results.Length];
+                for (int i = 0; i < results.Length; ++i)
+                    components[i] = (T)(object)results[i];
+            }
+            catch (Exception)
+            {
+                components = null;
+                Debug.LogError($"No component of type {components.GetType()} with tag {tag} is found under actor {actor.name}, attached on the game object {actor.gameObject.name}. Try assigning the component with the right Tag");
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool TryGetComponentsInParent<T>(this Actor actor, out T[] components, string tag = "", bool includeInactive = false)
+        {
+            var results = actor.GetComponentsInParent(typeof(T), tag, includeInactive);
+            try
+            {
+                if (results.Length == 0)
+                    results = null;
+
+                components = new T[results.Length];
+                for (int i = 0; i < results.Length; ++i)
+                    components[i] = (T)(object)results[i];
+            }
+            catch (Exception)
+            {
+                components = null;
+                Debug.LogError($"No component of type {components.GetType()} with tag {tag} is found in parent of actor {actor.name}, attached on the game object {actor.gameObject.name}. Try assigning the component with the right Tag");
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool TryGetComponentInParent<T>(this Actor actor, out T component, string tag = "", bool includeInactive = false)
+        {
+            component = (T)(object)actor.GetComponentInParent(typeof(T), tag, includeInactive);
+            return component != null;
+        }
+
+        public static Component GetComponentInParent(this Actor actor, Type type, string tag = "", bool includeInactive = false)
+        {
+            var obj = TaggerExtensions.GetComponentInParent(actor, type, tag, includeInactive);
+            if (obj == null)
+                Debug.LogError($"No component of type {type.GetType()} with tag {tag} is found under actor {actor.name}, attached on the game object {actor.gameObject.name}. Try assigning the component with the right Tag");
+
+            return obj;
+        }
+
+        public static T Get<T>(this Actor actor, string tag = "")
+        {
+            if (actor.TryGet<T>(out var service, tag))
+                return service;
+
+            return default(T);
+        }
+
+        public static void Add<T>(this Actor actor, object service, string tag = "")
+        {
+            ComponentExtensions.serviceLocator.Add<T>(service, tag);
+        }
+
+        public static T GetOrCreate<T>(this Actor actor, string tag = "")
+        {
+            return ComponentExtensions.serviceLocator.GetOrCreate<T>(tag);
+        }
+
+        public static bool Remove<T>(this Actor actor, string tag = "")
+        {
+            return ComponentExtensions.serviceLocator.Remove<T>(tag);
+        }
+    }
+}
