d8611bcc7398d85725e99502ddc7674435fd02b8
Abhi86Shek <sonkar.abhishek86@gmail.com>
2024-01-14 14:14:04 +0530

    updated animatior parameter config

--- Runtime/Utilities/AnimatorParameterConfig.cs
+++ Runtime/Utilities/AnimatorParameterConfig.cs
@@ -1,10 +1,37 @@
 using UnityEngine;
+using System;
 
 namespace SAS.StateMachineGraph.Utilities
 {
     [CreateAssetMenu(menuName = "SAS/State Machine Character Controller/Animator Parameters Config")]
-    public class AnimatorParameterConfig : ParameterConfigBase
+    public class AnimatorParameterConfig : ScriptableObject
     {
+        [System.Serializable]
+        public class ParametersKeyMap
+        {
+            public string key;
+            public Parameter[] parameters;
+            public string awaitableStateTag;
+            public float startDelay;
+        }
+
+        [SerializeField] private ParametersKeyMap[] m_ParametersKeyMap;
+
+#if UNITY_EDITOR
+        [SerializeField, TextArea] private string m_Description;
+#endif
+
+        public bool TryGet(string key, out ParametersKeyMap parametersKeyMap)
+        {
+            parametersKeyMap = Array.Find(m_ParametersKeyMap, ele => ele.key.Equals(key, StringComparison.OrdinalIgnoreCase));
+            if (parametersKeyMap == null)
+            {
+                Debug.LogWarning($"No parameters has been found wrt the key:  {key} under the Parameter config SO : {this.name}");
+                return false;
+            }
+            return true;
+        }
+
         public void ApplyParameters(in Animator animator, in string key)
         {
             if (TryGet(key, out var parametersKeyMap))
