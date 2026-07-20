5a3e500e5e2a11b62fc3ca75b70bd983e4660086
Abhishek Sonkar <sonkar.abhishek86@gmail.com>
2026-07-21 00:08:49 +0530

    clean up

--- Editor/StateMachineModelOpenAsset.cs
+++ Editor/StateMachineModelOpenAsset.cs
@@ -8,7 +8,11 @@ namespace SAS.StateMachineGraph.Editor
         [OnOpenAsset(1)]
         public static bool OpenStateMachineGraph(int instanceID, int line)
         {
+#if UNITY_6000_3_OR_NEWER
             var stateMachineController = EditorUtility.EntityIdToObject(instanceID);
+#else
+         var stateMachineController = EditorUtility.InstanceIDToObject(instanceID);
+#endif
             if(stateMachineController.GetType() == typeof(RuntimeStateMachineController))
                 StateMachineEditorWindow.ShowBehaviourGraphEditor(stateMachineController as RuntimeStateMachineController);
             return false;
